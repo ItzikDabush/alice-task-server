f8a318897f5d865c04f70e898527737d04b2cba9
Itzik Dabush <itikdb@gmail.com>
2019-10-14 21:29:47 +0300

    fixed the perc bug

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ function getRelevantData(response) {
   // according to OpenWeatherMap they don't provide probability of precipitation at the moment just the precipitation im mm unit for the past 1h or 3hr.
   
   //Since OpenWeatherMap respone with rain perciptation only if there is a rain precipitation, I used a logical opertor to prevent undefined value in case of no rain.
+  console.log(response.data.rain)
   const precipitation = response.data.rain ? response.data.rain : { "1h": "0" };
 
   return {
