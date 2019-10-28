4a5a10b6c8af2b0b3fd7a84196283672ebd1cdce
Itzik Dabush <itikdb@gmail.com>
2019-10-28 20:09:22 +0200

    updatinf gitignore and apikey

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const apiKey = "866559263bd4a6368cc34806cba6f97d";
+const apiKey = process.env.API_KEY;
 
 app.use(cors());
 app.use(bodyParser.json());
